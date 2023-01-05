0bc1f6c7142ad7222cdc715a84f085a5102dd221
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-01-05 13:04:22 +0200

    Add debug log for event sending.

--- server/src/utils/WebSocket.ts
+++ server/src/utils/WebSocket.ts
@@ -3,9 +3,12 @@ import WebSocket from 'ws'
 import logger from './logger'
 
 export const sendWebSocketEvent = async (server: WebSocket.Server, data: unknown) => {
+  const strData = typeof data === 'string' ? data : JSON.stringify(data)
+  logger.debug(`Sending data ${strData} to ${server.clients.values.length} socket clients `)
+  // TODO: client register with client specific events
   server.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      typeof data === 'string' ? client.send(data) : client.send(JSON.stringify(data))
+      client.send(strData)
     }
   })
 }
