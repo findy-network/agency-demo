4ab1fa47ea163b7f328a05378110901329155b7d
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-01-05 13:08:01 +0200

    Fix debug print.

--- server/src/utils/WebSocket.ts
+++ server/src/utils/WebSocket.ts
@@ -4,7 +4,7 @@ import logger from './logger'
 
 export const sendWebSocketEvent = async (server: WebSocket.Server, data: unknown) => {
   const strData = typeof data === 'string' ? data : JSON.stringify(data)
-  logger.debug(`Sending data ${strData} to ${server.clients.values.length} socket clients `)
+  logger.debug(`Sending data ${strData} to ${server.clients.size} socket clients `)
   // TODO: client register with client specific events
   server.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
